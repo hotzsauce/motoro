b17fb88147990e47ca995df7b5771458b45e33b1
Logan Hotz <logan@modo.energy>
2025-07-22 11:11:12 -0400

    fixed typos

--- motoro/viz/__init__.py
+++ motoro/viz/__init__.py
@@ -4,14 +4,14 @@ To use the plotly templates, use the following boilerplate code
 pd.options.plotting.backend = "plotly"
 
 import plotly.graph_objects as go
-import plotly.io as io
+import plotly.io as pio
 
 from motoro.viz import dark, light
 
 pio.templates["modo-dark"] = go.layout.Template(layout=dark)
 pio.templates["modo-light"] = go.layout.Template(layout=light)
 
-# pio.templates.default = "modo-light" # or "modo-dark"
+pio.templates.default = "modo-light" # or "modo-dark"
 """
 try:
     from motoro.viz.dark import modo_dark_layout as dark
